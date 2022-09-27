7f045ced20b8761b06b076bfb5c8b103bd0e752c
NobbyN0bbs <105239455+NobbyN0bbs@users.noreply.github.com>
2022-09-27 09:03:25 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,17 +2,8 @@ basic.forever(function () {
     SCD40.start_continuous_measurement()
     basic.showString("CO2")
     basic.showString("" + Math.round(SCD40.get_co2()))
-    basic.showString("ppm")
     basic.showString("TEMP")
-    basic.showString("" + Math.round(SCD40.get_temperature(SCD40.SCD40_T_UNIT.C)))
-    images.createBigImage(`
-        . # # # . . # # . .
-        . # . # . # . . . .
-        . # # # . # . . . .
-        . . . . . # . . . .
-        . . . . . . # # . .
-        `).scrollImage(1, 200)
+    basic.showString("" + Math.round(SCD40.get_temperature(SCD40.SCD40_T_UNIT.C) * 10) / 10)
     basic.showString("HUM")
     basic.showString("" + Math.round(SCD40.get_relative_humidity()))
-    basic.showString("%")
 })
